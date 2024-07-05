242a0b721932ddcdbd66fbecfb03e79744d6a14b
Unal Focadan <unal.focadan@gmail.com>
2024-07-05 17:44:39 +0300

    05072024_Commit_v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,20 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './login/login.component.html',
+  styleUrl: './login/login.component.css'
 })
+
 export class AppComponent {
   title = 'myAngularLoginSignup';
+}
+
+export class LoginComponent {
+  email: any;
+  password: any;
+  title = 'myAngularLoginSignup';
+  onSubmit() {
+    console.log('Login:', this.email, this.password);
+    // Implement your login logic here
+  }
 }
